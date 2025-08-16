c066e8c188a5e6e28b1ff9d3b83dd2e98af86434
Aleksandr Chilikov <chas.prog@gmail.com>
2025-08-16 17:02:28 +0700

    Update MoviesRules

---
+++ etl_service/src/producers/base.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+from datetime import datetime
+from typing import Generic, TypeVar
+
+from pydantic import BaseModel
+from sqlalchemy.ext.asyncio import AsyncSession
+
+PDModelsT = TypeVar("PDModelsT", bound=BaseModel)
+
+
+class BaseRule(ABC, Generic[PDModelsT]):
+
+    @abstractmethod
+    async def run(
+        self,
+        session: AsyncSession,
+        ref_dt: datetime | None,
+    ) -> list[PDModelsT]:
+        pass
