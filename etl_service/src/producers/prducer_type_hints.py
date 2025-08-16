3df16f952bbc5eca52c8a840f9e30a1edbb80ca1
Aleksandr Chilikov <chas.prog@gmail.com>
2025-08-16 17:01:15 +0700

    Update MoviesRules

--- etl_service/src/producers/prducer_type_hints.py
+++ etl_service/src/producers/prducer_type_hints.py
@@ -1,13 +1,7 @@
-from typing import Mapping, Type, TypeVar
+from typing import TypeVar
 
 from models.pg_models import Base
-from producers.base import BaseRule
 from pydantic import BaseModel
 
 PGModelsT = TypeVar("PGModelsT", bound=Base)
 PDModelsT = TypeVar("PDModelsT", bound=BaseModel)
-
-ModelsByRuleT = Mapping[
-    Type[PGModelsT],
-    Type[BaseRule[BaseModel]],
-]
