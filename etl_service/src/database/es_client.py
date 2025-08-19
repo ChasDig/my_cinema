7bd09ecb325bf7a3c621603f1f11c949406202a2
Aleksandr Chilikov <chas.prog@gmail.com>
2025-08-19 16:11:59 +0700

    Fix AsyncESClient type-hints

--- etl_service/src/database/es_client.py
+++ etl_service/src/database/es_client.py
@@ -1,10 +1,10 @@
 import json
 from functools import wraps
-from typing import TypeVar
+from typing import Any, Awaitable, Callable, TypeVar
 
 from core.app_config import config, db_config
 from core.app_logger import logger
-from elasticsearch import AsyncElasticsearch, NotFoundError
+from elasticsearch import AsyncElasticsearch
 
 ESClient_T = TypeVar("ESClient_T", bound="AsyncESClient")
 
@@ -12,31 +12,34 @@ ESClient_T = TypeVar("ESClient_T", bound="AsyncESClient")
 class AsyncESClient:
     """Async-клиент Elasticsearch."""
 
-    def __init__(self):  # type: ignore
-        self._client = AsyncElasticsearch(
+    def __init__(self) -> None:
+        self._client: AsyncElasticsearch = AsyncElasticsearch(
             config.elastic_url,
             basic_auth=(config.elastic_name, config.elastic_password),
             max_retries=0,
             retry_on_timeout=False,
         )
 
-    async def close_(self):  # type: ignore
+    async def close_(self) -> None:
         await self._client.close()
 
-    async def create_index_with_ignore(self, index_, body=None):  # type: ignore  # noqa: E501
+    async def create_index_with_ignore(
+        self,
+        index_: str,
+        body: dict[str, Any] | None = None,
+    ) -> None:
         if body:
-            try:
-                await self._client.search(index=index_)
+            exists = await self._client.indices.exists(index=index_)
 
-            except NotFoundError:
+            if not exists:
                 await self._client.indices.create(index=index_, body=body)
 
-    async def insert_document(  # type: ignore
+    async def insert_document(
         self,
-        index_,
-        document,
-        id_,
-    ):
+        index_: str,
+        document: dict[str, Any],
+        id_: str,
+    ) -> dict[str, bool]:
         result = {"status": True}
         response_ = await self._client.index(
             index=index_,
@@ -44,36 +47,43 @@ class AsyncESClient:
             id=id_,
         )
 
-        if body := response_.get("body"):
-            if error := body.get("error"):
-                result["status"], result["message"] = False, error
+        if "error" in response_:
+            result["status"], result["message"] = False, response_.get("error")
 
         return result
 
 
 class ESContextManager:
-    """Контекстный менеджер по работе с RedisStorage."""
+    """Контекстный менеджер по работе с AsyncESClient."""
 
-    def __init__(self):  # type: ignore
-        self._client = None
+    def __init__(self) -> None:
+        self._client: None | AsyncESClient = None
 
     @property
-    def client(self):  # type: ignore
+    def client(self) -> AsyncESClient | None:
         return self._client
 
-    async def __aenter__(self):  # type: ignore
-        self._client = AsyncESClient()  # type: ignore
+    async def __aenter__(self) -> AsyncESClient:
+        self._client = AsyncESClient()
 
         return self._client
 
-    async def __aexit__(self, exc_type, exc_val, exc_tb):  # type: ignore
-        await self._client.close_()  # type: ignore
+    async def __aexit__(
+        self,
+        exc_type: str,
+        exc_val: Any,
+        exc_tb: str,
+    ) -> None:
+        if self._client is not None:
+            await self._client.close_()
 
 
-def es_indexes_mapping_checker(func):  # type: ignore
+def es_indexes_mapping_checker(
+    func: Callable[..., Awaitable[Any]],
+) -> Callable[..., Awaitable[Any]]:
     @wraps(func)
-    async def wrapper(*args, **kwargs):  # type: ignore
-        async with ESContextManager() as es_client:  # type: ignore
+    async def wrapper(*args: Any, **kwargs: Any) -> Any:
+        async with ESContextManager() as es_client:
             for index_name in db_config.es_indexes_name:
                 try:
                     with open(index_name) as fp:
